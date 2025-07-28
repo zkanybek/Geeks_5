b132b36d0b4a5e1ff9748bf163e60eb9b55c8f26
zkanybek <zkanybekov62@gmail.com>
2025-07-28 19:02:48 +0300

    HW: did hw7

--- shop_api/settings.py
+++ shop_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8#ksmuq5irrmq52-og#ox*al_tg@at9*8)$110=cjdzh8zf#io'
+SECRET_KEY = os.environ.get('SECRET_KEY_DB')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
